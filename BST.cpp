9c4836a050bbdc41ee51108c03fee22e02e1d23a
anvari1313 <ahmad.anv@gmail.com>
2016-11-24 12:26:59 +0330

    add empty implementation to make it compilable

--- BST.cpp
+++ BST.cpp
@@ -3,6 +3,10 @@
 //
 
 #include "BST.h"
+//////////////////////////////////////////////////
+///  Function Implementaoin of BSTNode class  ////
+//////////////////////////////////////////////////
+
 template <class type> BSTNode<type>::BSTNode()
 {
     this->leftChild = nullptr;
@@ -13,10 +17,14 @@ template <class type> BSTNode<type>::BSTNode(type &data)
     BSTNode();
     this->data = data;
 }
-template <class type> type BSTNode<type>::getData()
-{
-    return this->data;
-}
+template <class type> type BSTNode<type>::getData() { return this->data; }
+
+template <class type> void BSTNode<type>::setLeftChild(BSTNode<type> *lc) {}
+template <class type> void BSTNode<type>::setRightChild(BSTNode<type> *rc) {}
+template <class type> void BSTNode<type>::setData(type &data) {}
+template <class type> BSTNode<type> *BSTNode<type>::getLeftChild(){}
+template <class type> BSTNode<type> *BSTNode<type>::getRightChild(){}
+template <class type> type *BSTNode<type>::getData(type *dataPointer){}
 //////////////////////////////////////////////
 ///  Function Implementaoin of BST class  ////
 //////////////////////////////////////////////
@@ -26,7 +34,7 @@ template <class type> BST<type>::BST()
 }
 template <class type> void BST<type>::add(type &data)
 {
-    BSTNode **pointer = &(this->root);
+    BSTNode<type> **pointer = &(this->root);
     while ((*pointer)){
         if(data > (*pointer)->getData()){
             (*pointer) = ((*pointer)->getLeftChild());
@@ -34,10 +42,16 @@ template <class type> void BST<type>::add(type &data)
             (*pointer) = ((*pointer)->getRightChild());
         }
     }
-    (*pointer) = new BSTNode(data);
+    (*pointer) = new BSTNode<type>(data);
 }
 
 template <class type> BST<type>::~BST()
 {
     // Todo design an algorithm to free the memory
 }
+template <class type> void BST<type>::printPreOrder() {}
+template <class type> void BST<type>::printInOrder() {}
+template <class type> void BST<type>::printPostOrder() {}
+template <class type> void BST<type>::printPreOrder(BSTNode<type> *root){}
+template <class type> void BST<type>::printInOrder(BSTNode<type> *root){}
+template <class type> void BST<type>::printPostOrder(BSTNode<type> *root){}
