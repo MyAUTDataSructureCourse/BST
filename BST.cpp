a2dc4ecbfad788db7d4ee03e9fa12697d81cd4a9
Ahmad <ahmad.anv@gmail.com>
2016-12-01 22:04:20 +0330

    Implementing add function and (pre/in/post) order

--- BST.cpp
+++ BST.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "BST.h"
+
 //////////////////////////////////////////////////
 ///  Function Implementaoin of BSTNode class  ////
 //////////////////////////////////////////////////
@@ -17,41 +18,115 @@ template <class type> BSTNode<type>::BSTNode(type &data)
     BSTNode();
     this->data = data;
 }
-template <class type> type BSTNode<type>::getData() { return this->data; }
 
-template <class type> void BSTNode<type>::setLeftChild(BSTNode<type> *lc) {}
-template <class type> void BSTNode<type>::setRightChild(BSTNode<type> *rc) {}
-template <class type> void BSTNode<type>::setData(type &data) {}
-template <class type> BSTNode<type> *BSTNode<type>::getLeftChild(){}
-template <class type> BSTNode<type> *BSTNode<type>::getRightChild(){}
-template <class type> type *BSTNode<type>::getData(type *dataPointer){}
-//////////////////////////////////////////////
-///  Function Implementaoin of BST class  ////
-//////////////////////////////////////////////
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////  Function Implementaoin of BST class  //////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * If want to add the nodes to the tree with recursive implementation
+ * @tparam type
+ */
+//#define BST_ADD_RECURSIVE_IMPLEMENTATION
+
 template <class type> BST<type>::BST()
 {
     this->root = nullptr;
 }
-template <class type> void BST<type>::add(type &data)
+
+template <class type> void BST<type>::addHelper(BSTNode<type> *root, BSTNode<type> *node)
 {
-    BSTNode<type> **pointer = &(this->root);
-    while ((*pointer)){
-        if(data > (*pointer)->getData()){
-            (*pointer) = ((*pointer)->getLeftChild());
-        } else{     //  data <= (*pointer)->getLeftChild()->getData()
-            (*pointer) = ((*pointer)->getRightChild());
-        }
+    if(root->data > node->data){
+        if (root->leftChild)
+            addHelper(root->leftChild,node);
+        else
+            root->leftChild = node;
+    } else{
+        if (root->rightChild)
+            addHelper(root->rightChild,node);
+        else
+            root->rightChild = node;
     }
-    (*pointer) = new BSTNode<type>(data);
+}
+template <class type> void BST<type>::add(type data)
+{
+    BSTNode<type> *newNode = new BSTNode<type>(data);
+    newNode->leftChild = nullptr;
+    newNode->rightChild = nullptr;
+
+#ifdef BST_ADD_RECURSIVE_IMPLEMENTATION
+    if(root)
+        addHelper(root, newNode);
+    else
+        root = newNode;
+#endif
+
+#ifndef BST_ADD_RECURSIVE_IMPLEMENTATION
+    if(this->root){
+        BSTNode<type> *parent = nullptr;
+        BSTNode<type> *pointer = this->root;
+        while (pointer){
+            parent = pointer;
+            if(data > pointer->data){
+                pointer = (pointer->rightChild);
+            } else if(data < pointer->data){
+                pointer = (pointer->leftChild);
+            }else{
+                // TODO something for handling if two nodes have equal data
+                std::cout << "Some wrong has happened" << std::endl;
+                return;
+            }
+        }
+        if(data < parent->data){
+            parent->leftChild = newNode;
+        } else if(data > parent->data) {
+            parent->rightChild = newNode;
+        }
+    } else{
+        this->root = newNode;
+    }
+#endif
 }
 
 template <class type> BST<type>::~BST()
 {
     // Todo design an algorithm to free the memory
 }
-template <class type> void BST<type>::printPreOrder() {}
-template <class type> void BST<type>::printInOrder() {}
-template <class type> void BST<type>::printPostOrder() {}
-template <class type> void BST<type>::printPreOrder(BSTNode<type> *root){}
-template <class type> void BST<type>::printInOrder(BSTNode<type> *root){}
-template <class type> void BST<type>::printPostOrder(BSTNode<type> *root){}
+template <class type> void BST<type>::printPreOrder(std::ostream &cout)
+{
+    this->printPreOrder(this->root, cout);
+}
+template <class type> void BST<type>::printInOrder(std::ostream &cout)
+{
+    this->printInOrder(this->root, cout);
+}
+template <class type> void BST<type>::printPostOrder(std::ostream &cout)
+{
+    this->printPostOrder(this->root, cout);
+}
+template <class type> void BST<type>::printPreOrder(BSTNode<type> *root, std::ostream &cout)
+{
+    if(root == nullptr)
+        return;
+    cout << root->data << std::endl;
+    this->printPreOrder(root->leftChild, cout);
+    this->printPreOrder(root->rightChild, cout);
+}
+template <class type> void BST<type>::printInOrder(BSTNode<type> *root, std::ostream &cout)
+{
+    if(root == nullptr)
+        return;
+    this->printInOrder(root->leftChild, cout);
+    cout << root->data << std::endl;
+    this->printInOrder(root->rightChild, cout);
+}
+template <class type> void BST<type>::printPostOrder(BSTNode<type> *root, std::ostream &cout)
+{
+    if(root == nullptr)
+        return;
+    this->printPostOrder(root->leftChild, cout);
+    this->printPostOrder(root->rightChild, cout);
+    cout << root->data << std::endl;
+}
